415aeb3e90cf3f91ef24c4b583f72a0435d7a0af
zhougaoxiong.newbeiyang <zhougaociongcall@163.com>
2019-06-06 17:55:47 +0800

    add grammar

---
+++ Test/pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+
+  onLoad: function (options) {
+    // 页面初始化
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面推到后台
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+
+})
